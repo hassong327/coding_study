b17948c06ebfaa13f76817daf55b73b5f784e0e6
hassong327 <127066019+hassong327@users.noreply.github.com>
2026-08-06 22:40:37 +0900

    Time: 5 ms (12.49%), Space: 12.6 MB (42.72%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -12,10 +12,13 @@ class Solution(object):
             arr[num] += 1
 
         # 내가 찾으려는건 [i:] 범위의 value 합이 i보다 큰 i들 중에서 가장 큰 i
+
         # cnt는 끝부터 누적합을 위해서
         cnt = 0
+        # 뒤부터 탐색 시작
         for i in range(1000, 0, -1):
             cnt += arr[i]
+            # i를 넘는 순간이 가장 큰 i 니까 return
             if cnt >= i:
                 return i
 
