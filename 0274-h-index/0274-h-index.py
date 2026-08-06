9dee7805a7c63f1c48a9038e671b06ca765b56a7
hassong327 <127066019+hassong327@users.noreply.github.com>
2026-08-06 22:38:23 +0900

    Time: 4 ms (20.13%), Space: 12.5 MB (76.35%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -4,14 +4,17 @@ class Solution(object):
         :type citations: List[int]
         :rtype: int
         """
-        arr = [0]*1001
 
+        # 인용수만큼 칸 만들고, 그 인용수에 해당하는 칸에 해당 인용수의 논문이 몇 개인지 적기
+        # 0은 제외, 1부터 1000까지
+        arr = [0]*1001 
         for num in citations:
             arr[num] += 1
 
+        # 내가 찾으려는건 [i:] 범위의 value 합이 i보다 큰 i들 중에서 가장 큰 i
+        # cnt는 끝부터 누적합을 위해서
         cnt = 0
-
-        for i in range(1000, -1, -1):
+        for i in range(1000, 0, -1):
             cnt += arr[i]
             if cnt >= i:
                 return i
