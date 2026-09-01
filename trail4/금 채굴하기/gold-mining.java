37936ecbcde9f0baac49ba9df763e5d93a1e1810
hassong327 <127066019+hassong327@users.noreply.github.com>
2026-09-01 21:37:38 +0900

    [Passed] 금 채굴하기 (127ms, 10MB)

--- trail4/금 채굴하기/gold-mining.java
+++ trail4/금 채굴하기/gold-mining.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int n = 0;
-    /*
+
     private static class Point{
         int row;
         int col;
@@ -15,9 +15,55 @@ public class Main {
     }
 
     static ArrayList<Point> visit = new ArrayList<>();
-    */
-    static int[] dr = {1, -1, 0, 0};
-    static int[] dc = {0, 0, 1, -1};
+    
+    // 있으니까 쓰는데, 원래였으면 boolean으로 처리하는게 나음. boolean의 2차원 배열
+    static int[][] bfs(int[][] visited, int r, int c, int k){
+        Deque<Point> dq = new ArrayDeque<>();
+        int[][] dist = new int[n][n];
+        for(int i = 0 ; i < n ; i ++ ){
+            for(int j = 0 ; j < n ; j ++){
+                dist[i][j]= -1;
+            }
+        }
+
+        dq.offer(new Point(r, c));
+        visited[r][c] = 0;
+        dist[r][c] = 0;
+
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
+
+        
+        while(!dq.isEmpty()){
+            Point tmp = dq.poll();
+            int inner_r = tmp.row;
+            int inner_c = tmp.col;
+
+            if(dist[inner_r][inner_c] == k){
+                continue;
+            }
+            
+            for(int i = 0; i < 4; i++){
+                if(inner_r+dr[i]<0 || inner_r+dr[i]>=n || inner_c+dc[i]<0 || inner_c+dc[i]>=n){
+                    continue;
+                }
+                
+                int next_r = inner_r + dr[i];
+                int next_c = inner_c + dc[i];
+
+                if(visited[next_r][next_c]==-1){
+                    visited[next_r][next_c] = 1;
+                    dist[next_r][next_c] = dist[inner_r][inner_c]+1;
+                    dq.offer(new Point(next_r, next_c));
+                }
+            }
+            
+        }
+        return dist;
+    }
+
+
+
     static void dfs(int cnt, int r, int c, int k, int[][] visited) {
         if (cnt > k) {
             return;
@@ -35,9 +81,9 @@ public class Main {
             //visit.add(new Point(r, c));
             visited[r][c] = cnt;
         }
-
-
-
+        
+        int[] dr = {1, -1, 0, 0};
+        int[] dc = {0, 0, 1, -1};
         for (int i = 0; i < 4; i++) {
             int nr = r + dr[i];
             int nc = c + dc[i];
@@ -76,7 +122,8 @@ public class Main {
                     //visit.clear();
                     int gold = 0;
                     int cnt = 0;
-                    dfs(cnt, row, col, i, visited);
+                    //dfs(cnt, row, col, i, visited);
+                    int[][] result_arr = bfs(visited, row, col, i);
                     //System.out.printf("row = %d, col = %d", row, col);
                     //System.out.println("");
                     
@@ -90,7 +137,7 @@ public class Main {
                     */
                     for(int r = 0; r < n; r ++){
                         for(int c = 0; c < n; c++){
-                            if(visited[r][c]!=-1 && grid[r][c]==1){
+                            if(result_arr[r][c]>=0 && grid[r][c]==1){
                                 gold += 1;
                                 //System.out.println(gold);
                             }
