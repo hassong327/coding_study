adb950d41c14b1450581de8980e6524099bfc143
hassong327 <127066019+hassong327@users.noreply.github.com>
2026-09-03 22:31:55 +0900

    [Passed] 기울어진 직사각형 (138ms, 10MB)

--- trail4/기울어진 직사각형/slanted-rectangle.java
+++ trail4/기울어진 직사각형/slanted-rectangle.java
@@ -22,23 +22,31 @@ public class Main {
                 for(int i = row+1; i < n-1; i++) {
                     int bcol = total -i;
                     
+                    //밖으로 벗어날 경우
                     if(bcol < 0) {
                         break;
                     }
                     
+                    // 시작 지점 기준 좌하향 점 B
                     arr3.add(new int[] {i, bcol});
                     for(int j = i + 1; j < n; j++) {
+                        
+                        //B 부터 C 까지의 거리 -> 나중에 시작 지점에서 D 지점을 알 수 있음
                         int dist = j - i;
-                        int crow = j;
+                        
+                        // B 기준 우하향 점 C
                         int ccol = bcol + dist;
                         
+                        //밖으로 벗어날 경우
                         if(ccol >= n) {
                             break;
                         }
                         
+                        // C 기준 우상향 점 D
                         int drow = row + dist;
                         int dcol = col + dist;
                         
+                        // 밖으로 벗어날 경우
                         if(drow >= n || dcol >= n) {
                             break;
                         }
@@ -46,19 +54,27 @@ public class Main {
                         ArrayList<int[]> arr4 = new ArrayList<>(arr3);
                         arr4.add(new int[] {row, col});
                         
+                        // B to C
                         for(int step = 1; step <= dist; step ++) {
                             arr4.add(new int[] {i + step, bcol + step});
                         }
                         
+                        // A to D
                         for(int step = 1; step <= dist; step++) {
                             arr4.add(new int[] {row + step, col + step});
                         }
                         
+                        // D to B
                         int arr3_len = i - row;
                         
                         for(int step = 1; step < arr3_len; step++) {
                             arr4.add(new int[] {drow + step, dcol - step});
                         }
+                        
+                        // A to B 는 어딨지?
+                        // 앞에서의 arr3.add(new int[] {i, bcol}); 에서 넣어줌
+                        
+                        
                         
                         int tmp_sum = 0;
                         for(int[] target : arr4) {
